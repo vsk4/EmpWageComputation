48605495ca1cd21f631643c650720ef86023f127
vsk4 <saikumarvadla4@gmail.com>
2020-09-30 10:56:03 +0530

    [VSK]:Refractor added PartTimeEmployee

--- Empwageproblem/Empwageproblem/Program.cs
+++ Empwageproblem/Empwageproblem/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace EmployeeWages
 {
     class Program
@@ -8,14 +7,19 @@ namespace EmployeeWages
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
