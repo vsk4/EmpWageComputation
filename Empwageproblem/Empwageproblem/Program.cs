943b682261c3582f0a75c9835ab963f92521cd40
vsk4 <saikumarvadla4@gmail.com>
2020-09-30 10:47:25 +0530

    [VSK]:Refractor added DailyEmployeeWage

--- Empwageproblem/Empwageproblem/Program.cs
+++ Empwageproblem/Empwageproblem/Program.cs
@@ -8,16 +8,21 @@ namespace EmployeeWages
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage :" + empWage);
             Console.ReadKey();
         }
     }
