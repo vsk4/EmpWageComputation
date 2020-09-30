6e1195f6970b176f72cc71b6805137e86884f343
vsk4 <saikumarvadla4@gmail.com>
2020-09-30 10:29:47 +0530

    [VSK]:Refractor added EmployeeAttendance

---
+++ Empwageproblem/Empwageproblem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            Console.ReadKey();
+        }
+    }
+}
