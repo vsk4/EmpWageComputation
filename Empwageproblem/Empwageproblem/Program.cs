9ac44a329a36cbc52efb9e50375cc72abc1200fd
vsk4 <saikumarvadla4@gmail.com>
2020-09-30 11:08:23 +0530

    [VSK]Regractor Added Conditional wages

--- Empwageproblem/Empwageproblem/Program.cs
+++ Empwageproblem/Empwageproblem/Program.cs
@@ -7,17 +7,20 @@ namespace EmployeeWages
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
-
             int empHrs = 0;
             int empWage = 0;
             int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
             Random random = new Random();
-            for (int i = 0; i < NUM_OF_WORKING_DAYS; i++)
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
                 int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
                 switch (empCheck)
                 {
                     case 2:
@@ -30,12 +33,10 @@ namespace EmployeeWages
                         empHrs = 0;
                         break;
                 }
-
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Employee Wage :" + empWage);
-                Console.ReadKey();
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp hrs " + empHrs);
             }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
