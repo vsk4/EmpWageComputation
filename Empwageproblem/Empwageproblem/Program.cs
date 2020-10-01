04c792ecde3af1a9035ec25efc474f2b64178f94
vsk4 <saikumarvadla4@gmail.com>
2020-10-01 08:33:53 +0530

    [VSK]:Regractor added MultipleCompanies

--- Empwageproblem/Empwageproblem/Program.cs
+++ Empwageproblem/Empwageproblem/Program.cs
@@ -3,41 +3,42 @@ namespace EmployeeWages
 {
     class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HRS_IN_MONTH = 10;
-        public static int computeEmpWage()
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            Console.WriteLine("Welcome to Employee Wage Computation Problem on Master Branch");
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            Random random = new Random();
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
-                totalWorkingDays++;
-                Random random = new Random();
                 int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
                 switch (empCheck)
                 {
-                    case IS_PART_TIME:
+                    case 2:
                         empHrs = 4;
                         break;
-                    case IS_FULL_TIME:
+                    case 1:
                         empHrs = 8;
                         break;
                     default:
                         empHrs = 0;
                         break;
                 }
-                totalEmpHrs = totalEmpHrs + empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp hrs " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
             return totalEmpWage;
         }
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            computeEmpWage();
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
